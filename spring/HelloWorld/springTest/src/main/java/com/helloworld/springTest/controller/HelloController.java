bcea09a2261f4aa40289ebdb50842bcd8f985e1e
nubiaferr <n.ferr@hotmail.com>
2021-07-23 20:21:12 -0300

    first tests with springboot

---
+++ spring/HelloWorld/springTest/src/main/java/com/helloworld/springTest/controller/HelloController.java
@@ -0,0 +1,32 @@
+package com.helloworld.springTest.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+	@GetMapping
+	public String hello() {
+		return "<h1 style='text-align: center; font-size: 150px; color: #f542b6; margin-top: 8%;'>Hello World!</h1>"+
+				"<p style='margin: 0 auto; width: 500px; text-align: center; font-size: 50px;'>Today I learned about growth mindset and attention to details.</p>";
+	}
+	
+}
+
+/*
+ * public class HelloController {
+	@GetMapping
+	public String hello() {
+		return "<h1 style='text-align: center; font-size: 150px; color: #f542b6; margin-top: 8%;'>Hello World!</h1>";
+	}
+	
+	@GetMapping
+	public String text(){
+		return "<p style='margin: 0 auto; width: 500px; text-align: center; font-size: 50px;'>Today I learned about growth mindset and attention to details.</p>";
+	}
+	
+}
+
+ */
